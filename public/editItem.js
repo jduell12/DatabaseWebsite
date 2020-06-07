4c9c4f375f7918abfe34dfc69884327c092c18c3
Jessica <duellje@oregonstate.edu>
2020-06-06 22:28:08 -0400

    everything working and style set besides edit button

--- public/editItem.js
+++ public/editItem.js
@@ -4,7 +4,7 @@ function editItem(item_Num){
         type: 'PUT',
         data: $('#editItem').serialize(),
         success: function(result){
-           window.location.replace("../inventory.handlebars" + item_Num);
+        //    window.location.replace("../inventory.handlebars" + item_Num);
         }
     })
 };
