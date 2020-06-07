3d107f8d069134c20f4a3dbe2d3cce6ff3c44126
Jessica <duellje@oregonstate.edu>
2020-06-06 22:07:35 -0400

    changed editItem.js to go to inventory on edit

--- public/editItem.js
+++ public/editItem.js
@@ -4,18 +4,7 @@ function editItem(item_Num){
         type: 'PUT',
         data: $('#editItem').serialize(),
         success: function(result){
-           window.location.replace("editItem");
+           window.location.replace("../inventory.handlebars" + item_Num);
         }
     })
 };
-
-// function updateItem(item_Num){
-//     $.ajax({
-//         url: '/editItem/' + item_Num,
-//         type: 'PUT',
-//         data: $('#updateItem').serialize(),
-//         success: function(result){
-//             window.location.replace("../inventory");
-//         }
-//     })
-// };
