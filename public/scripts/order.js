894e939e3b901d40565f3ad61ff67c9ab00b2547
Jessica <duellje@oregonstate.edu>
2020-06-10 10:25:17 -0400

    got order table to load correctly

--- public/scripts/order.js
+++ public/scripts/order.js
@@ -14,206 +14,234 @@ let tableInfo = {
 };
 
 function getAllOrders(){
-    console.log('in order');
     let getRequest = httpRequest('GET', '/order/api', {});
     getRequest.then(function(result) {
-        console.log(result);
-        // updateTable(result);
+        updateTable(result);
     }).catch(function(err){
         console.log(err);
     })
 };
 
-// function updateTable(result){
-//     let data = JSON.parse(result.responseText).payload;
-//     tableInfo['data'] = data;
-//     generateTable(tableInfo);
-// }
+function updateTable(result){
+    let data = JSON.parse(result.responseText).payload;
+    tableInfo['data'] = data;
+    generateTable(tableInfo);
+}
 
-// function generateTable(tableData){
-//     let table = document.getElementById('table-body');
-//     let header = document.createElement('thead');
-//     let body = document.createElement('tbody');
-//     table.innerHTML = "";
+function generateTable(tableData){
+    let table = document.getElementById('table-body');
+    let header = document.createElement('thead');
+    let body = document.createElement('tbody');
+    table.innerHTML = "";
 
-//     let headerArray = tableData['columnHeader']
-//     let colomnOrderArray = tableData['columnOrder'];
-//     let dataArray = tableData['data'];
+    let headerArray = tableData['columnHeader']
+    let colomnOrderArray = tableData['columnOrder'];
+    let dataArray = tableData['data'];
 
-//     headerArray.forEach((item, index) => {
-//         let column = document.createElement('th');
-//         column.textContent = tableData['columnHeader'][index];
-//         header.appendChild(column);
-//     })
+    headerArray.forEach((item, index) => {
+        let column = document.createElement('th');
+        column.textContent = tableData['columnHeader'][index];
+        header.appendChild(column);
+    })
 
-//     for(let i = 0; i < dataArray.length; i++){
-//         let row = document.createElement('tr');
-//         row.className = 'tr';
-//         let id = tableData['data'][i][tableData['columnOrder'][0]['id']];
-//         row.setAttribute('id', id);
-//         let rowNum = 0;
+    for(let i = 0; i < dataArray.length; i++){
+        let row = document.createElement('tr');
+        row.className = 'tr';
+        let id = tableData['data'][i][tableData['columnOrder'][0]['id']];
+        row.setAttribute('id', id);
+        let rowNum = 0;
 
-//         for(let j = 0; j < colomnOrderArray.length; j++){
-//             let key = tableData['columnOrder'][j]['id'];
-//             let cell = document.createElement('td');
-//             cell.className = 'td';
-//             cell.id = key + '-' + id;
-//             cell.textContent = key == 'actions' ? '': tableData['data'][i][key];
+        for(let j = 0; j < colomnOrderArray.length; j++){
+            let key = tableData['columnOrder'][j]['id'];
+            let cell = document.createElement('td');
+            cell.className = 'td';
+            cell.id = key + '-' + id;
+            if(key === 'customer'){
+                cell.textContent = tableData['data'][i]['first_Name'] + ' ' + tableData['data'][i]['last_Name']
+            } else if(key === 'order_Complete' || key === 'order_Shipped'){
+                if (tableData['data'][i][key] === 1){
+                    cell.textContent = "Yes";
+                }else {
+                    cell.textContent = "No";
+                }
+            } else if(key === 'order_Date'){
+                let dateArray = tableData['data'][i][key].split('-');
+                let day = dateArray[2].split('T');
 
-//             if(j == 0){
-//                 cell.style.display = "none";
-//                 cell.classList.remove('td');
-//             }
-//             row.appendChild(cell);
-//         }
+                cell.textContent = dateArray[1] + '-' + day[0] + '-' + dateArray[0];
+            }else {
+                cell.textContent = key == 'actions' ? '': tableData['data'][i][key];
+            }
+           
 
-//         //creates delete button
-//         let deleteButton = document.createElement('button');
-//         deleteButton.textContent = "Delete";
-//         deleteButton.setAttribute('id', 'delete-' + id);
-//         deleteButton.addEventListener('click', deleteItem.bind(this, id));
-//         row.appendChild(deleteButton);
+            if(j == 0){
+                cell.style.display = "none";
+                cell.classList.remove('td');
+            }
+            row.appendChild(cell);
+        }
 
-//         //creates edit button
-//         let editButton = document.createElement('button');
-//         editButton.textContent = "Edit";
-//         editButton.setAttribute('id', 'edit-' + id);
-//         editButton.addEventListener('click', editItem.bind(this, id, tableInfo['columnOrder'][rowNum]['edit'], tableInfo['columnOrder']), false);
-//         row.appendChild(editButton);
+        //creates delete button
+        let deleteButton = document.createElement('button');
+        deleteButton.textContent = "Delete";
+        deleteButton.setAttribute('id', 'delete-' + id);
+        deleteButton.addEventListener('click', deleteItem.bind(this, id));
+        row.appendChild(deleteButton);
 
-//         //creates hidden update button
-//         let update = document.createElement('button');
-//         update.textContent = "Update";
-//         update.setAttribute('id', 'update-' + id);
-//         update.addEventListener('click', updateItem.bind(this, id));
-//         update.style.display = "none";
-//         row.appendChild(update);
+        //creates edit button
+        let editButton = document.createElement('button');
+        editButton.textContent = "Edit";
+        editButton.setAttribute('id', 'edit-' + id);
+        editButton.addEventListener('click', editItem.bind(this, id, tableInfo['columnOrder'][rowNum]['edit'], tableInfo['columnOrder']), false);
+        row.appendChild(editButton);
 
-//         body.appendChild(row);
-//         rowNum++;
-//     }
-//     table.appendChild(header);
-//     table.appendChild(body);
+        //creates hidden update button
+        let update = document.createElement('button');
+        update.textContent = "Update";
+        update.setAttribute('id', 'update-' + id);
+        update.addEventListener('click', updateItem.bind(this, id));
+        update.style.display = "none";
+        row.appendChild(update);
 
-//     if(!document.getElementById('search')){
-//         getSearchBar();
-//     }
-// }
+        body.appendChild(row);
+        rowNum++;
+    }
+    table.appendChild(header);
+    table.appendChild(body);
 
-// function editItem(id, callback, rowOrder){
+    if(!document.getElementById('search')){
+        getSearchBar();
+    }
+}
 
-//     rowOrder.forEach(item => {
-//         if(item['id'] !== 'item_Num'){
-//             let element = document.getElementById(item['id'] + '-' + id);
-//             let value = element.textContent || "";
-//             element.innerHTML = "";
+function editItem(id, callback, rowOrder){
 
-//             let input = document.createElement('input');
-//             input.setAttribute('type', item['type']);
-//             input.setAttribute('id', item['id'] + '-' + id + '-input');
-//             input.setAttribute('value', value);
-//             element.appendChild(input);
-//         }
-//     });
+    rowOrder.forEach(item => {
+        if(item['id'] !== 'item_Num'){
+            let element = document.getElementById(item['id'] + '-' + id);
+            let value = element.textContent || "";
+            element.innerHTML = "";
 
-//     let editButton = document.getElementById('edit-' + id);
-//     editButton.style.display = 'none';
+            let input = document.createElement('input');
+            input.setAttribute('type', item['type']);
+            input.setAttribute('id', item['id'] + '-' + id + '-input');
+            input.setAttribute('value', value);
+            element.appendChild(input);
+        }
+    });
 
-//     let updateButton = document.getElementById('update-' + id);
-//     updateButton.style.display = 'inline';
+    let editButton = document.getElementById('edit-' + id);
+    editButton.style.display = 'none';
+
+    let updateButton = document.getElementById('update-' + id);
+    updateButton.style.display = 'inline';
 
  
-// }
+}
 
-// function updateItem(id){
-//     let context = {};
-//     context['id'] = id;
+function updateItem(id){
+    let context = {};
+    context['id'] = id;
     
-//     for(let i = 0; i < tableInfo['columnOrder'].length; i++){
-//         let key = tableInfo['columnOrder'][i]['id'];
-//         if(key !== 'item_Num'){
-//             let element = document.getElementById(key + '-' + id + '-input');
-//             context[key] = element.value; 
-//         }
-//     }
+    for(let i = 0; i < tableInfo['columnOrder'].length; i++){
+        let key = tableInfo['columnOrder'][i]['id'];
+        if(key !== 'item_Num'){
+            let element = document.getElementById(key + '-' + id + '-input');
+            context[key] = element.value; 
+        }
+    }
 
-//     let editButton = document.getElementById('edit-' + id);
-//     editButton.style.display = 'inline';
+    let editButton = document.getElementById('edit-' + id);
+    editButton.style.display = 'inline';
 
-//     let updateButton = document.getElementById('update-' + id);
-//     updateButton.style.display = 'none';
+    let updateButton = document.getElementById('update-' + id);
+    updateButton.style.display = 'none';
 
-//     let putRequest = httpRequest('PUT', '/inventory/api', context);
+    if(context.order_Complete === 'Yes'){
+        context.order_Complete = 1;
+    }else {
+        context.order_Complete = 0;
+    }
 
-//     putRequest.then(function(result){
-//         updateTable(result);
-//     }).catch(function(err){
-//         console.log(err);
-//     })
-// }
+    if(context.order_Shipped === 'Yes'){
+        context.order_Shipped = 1;
+    } else {
+        context.order_Shipped = 0;
+    }
 
-// function deleteItem(id){
-//     let data = {};
-//     data.id = id;
-//     let deleteRequest = httpRequest('DELETE', '/inventory/api', data);
+    console.log(context);
 
-//     deleteRequest.then(function(result){
-//         updateTable(result);
-//     }).catch(function(err){
-//         console.log(err);
-//     })
+    // let putRequest = httpRequest('PUT', '/inventory/api', context);
 
-// }
-// function getSearchBar(){
-//     let tableTitle = document.querySelector('#table-title');
-//     let tableRows = document.querySelectorAll('.tr');
-//     let rowLengh = tableRows.length;
-//     let tableCells = document.querySelectorAll('.td');
-//     let cellLength = tableCells.length;
+    // putRequest.then(function(result){
+    //     updateTable(result);
+    // }).catch(function(err){
+    //     console.log(err);
+    // })
+}
 
-//     let searchBar = document.createElement('input');
-//     searchBar.setAttribute('type', 'text');
-//     searchBar.setAttribute('id', 'search');
-//     searchBar.placeholder = "Search for Items"
+function deleteItem(id){
+    let data = {};
+    data.id = id;
+    let deleteRequest = httpRequest('DELETE', '/inventory/api', data);
 
-//     searchBar.addEventListener('keyup', event => {
-//         let value = event.target.value;
-//         value = value.toUpperCase();
+    deleteRequest.then(function(result){
+        updateTable(result);
+    }).catch(function(err){
+        console.log(err);
+    })
 
-//         tableRows.forEach(row => {
-//             let td = row.childNodes;
-//             let tdArray = Array.from(td);
-//             let equals = true;
+}
+function getSearchBar(){
+    let tableTitle = document.querySelector('#table-title');
+    let tableRows = document.querySelectorAll('.tr');
+    let rowLengh = tableRows.length;
+    let tableCells = document.querySelectorAll('.td');
+    let cellLength = tableCells.length;
 
-//             let newTD = tdArray.filter(cell => {
-//                 if(cell.style.display !== 'none'){
-//                     if(cell.innerText !== 'Edit'){
-//                         if(cell.innerText !== 'Delete'){
-//                             return cell;
-//                         }
-//                     }
-//                 } 
-//             })
+    let searchBar = document.createElement('input');
+    searchBar.setAttribute('type', 'text');
+    searchBar.setAttribute('id', 'search');
+    searchBar.placeholder = "Search for Items"
 
-//             for(let i = 0; i < 4; i++){
-//                 if(newTD[i].innerText.toUpperCase().indexOf(value) > -1){
-//                     equals = false;
-//                 } 
-//             }
+    searchBar.addEventListener('keyup', event => {
+        let value = event.target.value;
+        value = value.toUpperCase();
 
-//             if(value === ""){
-//                 row.style.display = "";
-//             }else if(equals){
-//                 row.style.display ="none";
-//             }
+        tableRows.forEach(row => {
+            let td = row.childNodes;
+            let tdArray = Array.from(td);
+            let equals = true;
+
+            let newTD = tdArray.filter(cell => {
+                if(cell.style.display !== 'none'){
+                    if(cell.innerText !== 'Edit'){
+                        if(cell.innerText !== 'Delete'){
+                            return cell;
+                        }
+                    }
+                } 
+            })
+
+            for(let i = 0; i < 4; i++){
+                if(newTD[i].innerText.toUpperCase().indexOf(value) > -1){
+                    equals = false;
+                } 
+            }
+
+            if(value === ""){
+                row.style.display = "";
+            }else if(equals){
+                row.style.display ="none";
+            }
             
-//         })
+        })
         
         
-//     })
+    })
 
-//     tableTitle.appendChild(searchBar);
-// }
+    tableTitle.appendChild(searchBar);
+}
 
 function pageInit(){
     getAllOrders();
--- routes/order.js
+++ routes/order.js
@@ -9,13 +9,24 @@ router.route('/')
                 res.write(JSON.stringify(error));
                 res.end();
             }
-            console.log('in router');
-            console.log(results);
             res.json({'payload': results});
         });
     })
     .put(function(req, res){
-
+        console.log(req.body);
+        mysql.pool.query('UPDATE Orders SET order_Date = ?, payment_Type = ?, order_Complete = ?, order_Shipped = ? ;', [req.body.order_date, req.body.payment_Type, req.body.order_Complete, req.body.order_Shipped], function(err, results){
+            if(err){
+                res.write(JSON.stringify(err));
+                res.end();
+            }
+            mysql.pool.query("SELECT * FROM Orders INNER JOIN Customers ON Orders.fk_customer_Num = Customers.customer_Num;", function(error, results, fields){
+                if(error){
+                    res.write(JSON.stringify(error));
+                    res.end();
+                }
+                res.json({'payload': results});
+            });
+        })
     })
     .delete(function(req, res){
 
