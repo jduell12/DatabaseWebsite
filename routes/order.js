f6421739ce3421e1360b1289e56902d843301f22
Jessica <duellje@oregonstate.edu>
2020-06-10 11:08:28 -0400

    delete button working on orders

--- routes/order.js
+++ routes/order.js
@@ -13,7 +13,6 @@ router.route('/')
         });
     })
     .put(function(req, res){
-        console.log(req.body);
         mysql.pool.query('UPDATE Orders SET order_Date = ?, payment_Type = ?, order_Complete = ?, order_Shipped = ? WHERE order_Num = ?;', [req.body.order_Date, req.body.payment_Type, req.body.order_Complete, req.body.order_Shipped, req.body.id], function(err, results){
             if(err){
                 res.write(JSON.stringify(err));
@@ -21,7 +20,6 @@ router.route('/')
             }
         })
         mysql.pool.query("SELECT * FROM Orders INNER JOIN Customers ON Orders.fk_customer_Num = Customers.customer_Num;", function(error, results, fields){
-            console.log(results);
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
@@ -30,45 +28,19 @@ router.route('/')
         });
     })
     .delete(function(req, res){
-
+        mysql.pool.query("DELETE FROM Orders WHERE order_Num = ?;", [req.body.id], function(err){
+            if(err){
+                res.write(JSON.stringify(err));
+                res.end();
+            }
+        })
+        mysql.pool.query("SELECT * FROM Orders INNER JOIN Customers ON Orders.fk_customer_Num = Customers.customer_Num;", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            res.json({'payload': results});
+        });
     })
 
 module.exports = router;
-
-// module.exports = function(){
-//     const express = require('express');
-//     const router = express.Router();
- 
-
-//     router.get('/', function(req, res){
-//         let callBackCount = 0;
-//         let context = {};
-//         context.jsscripts = ["deleteOrder.js"];
-//         let mysql = req.app.get('mysql');
-//         getOrders(res, mysql, context, complete);
-//         function complete(){
-//             callBackCount++;
-//             if(callBackCount >= 1){
-//                 res.render('order', context);
-//             }
-//         }
-//     });
-
-//     /* Route to delete order from database, returns 202 upon success. Ajax will handle this. */
-//     router.delete('/:order_Num', function(req, res){
-//         let mysql = req.app.get('mysql');
-//         let sql = "DELETE FROM Orders WHERE order_Num = ?";
-//         let inserts = [req.params.order_Num];
-//         sql = mysql.pool.query(sql, inserts, function(error, results, fields){
-//             if(error){
-//                 res.write(JSON.stringify(error));
-//                 res.status(400);
-//                 res.end();
-//             }else{
-//                 res.render('order');
-//             }
-//         })
-//     });
-
-//     return router;
-// }();
